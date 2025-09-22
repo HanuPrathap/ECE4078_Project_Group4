94190ab30ee6c21838ed68c50e5390b1ffc8f039
jessicathenguyen47 <jessicathenguyen47@gmail.com>
2025-09-22 12:16:11 +1000

    Update gui.py
    Fix gui so that map and robot start facing north and integrate pibot camera. Also add parser arg so run command with groundtruth map file path input

--- Week07-08/gui.py
+++ Week07-08/gui.py
@@ -4,18 +4,26 @@
 # prints target fruits position in a specified order 
 # provides a skeletonn for naviagtion where you enter way points and the robot drives to them 
 
+# RUN python Week07-08/gui.py image_to_map_generator/generated_ground_truth_maps/final_output.txt
 
-ARENA_SIZE_M = 2.5
+
+ARENA_SIZE_M = 3   # need to change this 2.4 on the actual day
 WORLD_X_MIN, WORLD_X_MAX = -ARENA_SIZE_M / 2, ARENA_SIZE_M / 2
 WORLD_Y_MIN, WORLD_Y_MAX = -ARENA_SIZE_M / 2, ARENA_SIZE_M / 2
 
 PAD = 20
-PANEL_W = 480
-PANEL_H = 480
+PANEL_W = 720
+PANEL_H = 720
 MAP_ORIGIN_PX = (PAD, PAD)
 
-WIN_W = PANEL_W + 2 * PAD
-WIN_H = PANEL_H + 100
+
+CAM_W, CAM_H = 320, 240  # PiBot camera size
+MAP_W, MAP_H = PANEL_W, PANEL_H
+
+# Window width = map width + padding + camera width + padding
+WIN_W = MAP_W + PAD + CAM_W + 2*PAD
+WIN_H = max(CAM_H + 2*PAD, MAP_H + 2*PAD)
+
 
 GOAL_TOL_M = 0.2  # 20 cm tolerance
 
@@ -33,7 +41,7 @@ from dataclasses import dataclass
 
 
 # import SLAM components
-sys.path.insert(0, "C:/Users/jessi/Documents/2025/ECE4078/ECE4078_Project_Group4/Week07-08/slam")
+sys.path.insert(0, "C:/Users/jessi/Documents/2025/ECE4078/ECE4078_Project_Group4/Week07-08/slam") # need to modify this assuming Jas will run during demo
 
 from slam.ekf import EKF
 from slam.robot import Robot
@@ -41,7 +49,7 @@ import slam.aruco_detector as aruco
 
 # import utility functions
 sys.path.insert(0, "util")
-sys.path.insert(0, "C:/Users/jessi/Documents/2025/ECE4078/ECE4078_Project_Group4/Week07-08/util")
+sys.path.insert(0, "C:/Users/jessi/Documents/2025/ECE4078/ECE4078_Project_Group4/Week07-08/util") # need to modify this assuming Jas will run during demo
 
 
 from util.pibot import PenguinPi
@@ -71,8 +79,8 @@ def read_true_map(fname):
 
         # remove unique id of targets of the same type
         for key in gt_dict:
-            x = np.round(gt_dict[key]['x'], 1)
-            y = np.round(gt_dict[key]['y'], 1)
+            x = (np.round(gt_dict[key]['x'], 1))
+            y = (np.round(gt_dict[key]['y'], 1))
 
             if key.startswith('aruco'):
                 if key.startswith('aruco10'):
@@ -103,7 +111,7 @@ def read_search_list():
     @return: search order of the target fruits
     """
     search_list = []
-    fname = "C:/Users/jessi/Documents/2025/ECE4078/ECE4078_Project_Group4/Week07-08/search_list.txt"
+    fname = "C:/Users/jessi/Documents/2025/ECE4078/ECE4078_Project_Group4/Week07-08/search_list.txt"  # also need to change this for Jas
     with open(fname, 'r') as fd:
         fruits = fd.readlines()
 
@@ -219,6 +227,28 @@ class Pose:
     y: float = 0.0
     th: float = 0.0  # radians
 
+
+def rotate_ccw_90(x,y):
+    return y, -x
+
+
+def rotate_cw_90_px(u, v):
+    """Rotate a pixel coordinate 90° clockwise around the center of the map panel."""
+    cx = MAP_ORIGIN_PX[0] + PANEL_W / 2
+    cy = MAP_ORIGIN_PX[1] + PANEL_H / 2
+    
+    # Translate so center is origin
+    dx = u - cx
+    dy = v - cy
+    
+    # Rotate 90° clockwise
+    rx = dy
+    ry = dx
+    
+    # Translate back
+    return int(rx + cx), int(ry + cy)
+
+
 def px_to_world(u: int, v: int):
     x0, y0 = MAP_ORIGIN_PX
     if not (x0 <= u <= x0 + PANEL_W and y0 <= v <= y0 + PANEL_H):
@@ -226,12 +256,13 @@ def px_to_world(u: int, v: int):
     su = (u - x0) / PANEL_W
     sv = (v - y0) / PANEL_H
     x = WORLD_X_MIN + su * (WORLD_X_MAX - WORLD_X_MIN)
-    y = WORLD_Y_MAX - sv * (WORLD_Y_MAX - WORLD_Y_MIN)  # flip y
+    y = WORLD_Y_MAX - sv * (WORLD_Y_MAX - WORLD_Y_MIN)
     return float(x), float(y)
 
 
-def world_to_px(x: float, y: float):
-    x0, y0 = MAP_ORIGIN_PX
+def world_to_px(x: float, y: float, origin_px=MAP_ORIGIN_PX):
+    x0, y0 = origin_px
+    x0, y0 = rotate_ccw_90(x0,y0)
     su = (x - WORLD_X_MIN) / (WORLD_X_MAX - WORLD_X_MIN)
     sv = (WORLD_Y_MAX - y) / (WORLD_Y_MAX - WORLD_Y_MIN)
     u = int(x0 + su * PANEL_W)
@@ -239,54 +270,72 @@ def world_to_px(x: float, y: float):
     return u, v
 
 
-def draw_map(screen):
-    pygame.draw.rect(screen, (30, 30, 30), (*MAP_ORIGIN_PX, PANEL_W, PANEL_H))
-    pygame.draw.rect(screen, (90, 90, 90), (*MAP_ORIGIN_PX, PANEL_W, PANEL_H), 2)
+def draw_map(screen, origin_px=MAP_ORIGIN_PX):
+    x0, y0 = origin_px
+    pygame.draw.rect(screen, (30, 30, 30), (x0, y0, PANEL_W, PANEL_H))
+    pygame.draw.rect(screen, (90, 90, 90), (x0, y0, PANEL_W, PANEL_H), 2)
     for gx in np.linspace(WORLD_X_MIN, WORLD_X_MAX, 6):
-        u, _ = world_to_px(gx, 0.0)
+        u, _ = world_to_px(gx, 0.0, origin_px=origin_px)
         pygame.draw.line(screen, (55, 55, 55), (u, MAP_ORIGIN_PX[1]), (u, MAP_ORIGIN_PX[1]+PANEL_H))
     for gy in np.linspace(WORLD_Y_MIN, WORLD_Y_MAX, 6):
-        _, v = world_to_px(0.0, gy)
+        _, v = world_to_px(0.0, gy, origin_px=origin_px)
         pygame.draw.line(screen, (55, 55, 55), (MAP_ORIGIN_PX[0], v), (MAP_ORIGIN_PX[0]+PANEL_W, v))
 
-def draw_goal(screen, goal_xy):
+def draw_goal(screen, goal_xy, origin_px=MAP_ORIGIN_PX):
     if goal_xy is None:
         return
-    u, v = world_to_px(*goal_xy)
+    u, v = world_to_px(*goal_xy, origin_px = origin_px )
     # Draw the goal as a small yellow circle
     pygame.draw.circle(screen, (255, 200, 0), (u, v), 6)
     # Draw acceptance radius
     rad_px = int(GOAL_TOL_M / (WORLD_X_MAX - WORLD_X_MIN) * PANEL_W)
     pygame.draw.circle(screen, (255, 200, 0), (u, v), rad_px, 1)
 
-def draw_groundtruth_map(screen, fruits_true_pos, fruit_list, aruco_true_pos):
+def draw_groundtruth_map(screen, fruits_true_pos, fruit_list, aruco_true_pos, origin_px = MAP_ORIGIN_PX):
     """
     Draws fruits and ArUco markers on the 480x480 map.
     """
     # Draw fruits
     for i, pos in enumerate(fruits_true_pos):
-        u, v = world_to_px(pos[0], pos[1])
+        u, v = world_to_px(pos[0], pos[1], origin_px = origin_px)
+        u,v = rotate_cw_90_px(u,v)
+        #u,v = rotate_ccw_90(u,v)
         pygame.draw.circle(screen, (255, 165, 0), (u, v), 6)  # orange dot for fruits
         # optional: label
         font = pygame.font.SysFont("consolas", 14)
         label = font.render(fruit_list[i], True, (255, 255, 255))
         screen.blit(label, (u+5, v-5))
-    
+
     # Draw ArUco markers
     for i, pos in enumerate(aruco_true_pos):
-        u, v = world_to_px(pos[0], pos[1])
+        u, v = world_to_px(pos[0], pos[1], origin_px = origin_px)
+        # u,v = rotate_ccw_90(u,v)
+        u,v = rotate_cw_90_px(u,v)
         pygame.draw.rect(screen, (0, 255, 0), (u-4, v-4, 8, 8))  # green square for markers
         font = pygame.font.SysFont("consolas", 12)
         label = font.render(f"A{i+1}", True, (0,255,0))
         screen.blit(label, (u+5, v-5))
 
-def draw_robot(screen, pose: Pose):
-    u, v = world_to_px(pose.x, pose.y)
+def draw_robot(screen, pose: Pose, origin_px = MAP_ORIGIN_PX):
+    thet_rot = pose.th + math.pi/2
+    u, v = world_to_px(pose.x, pose.y, origin_px = origin_px)
     pygame.draw.circle(screen, (0, 180, 255), (u, v), 8)  # blue dot
-    hx = u + int(14 * math.cos(pose.th))
-    hy = v - int(14 * math.sin(pose.th))
+    hx = u + int(14 * math.cos(thet_rot))
+    hy = v - int(14 * math.sin(thet_rot))
     pygame.draw.line(screen, (0, 255, 180), (u, v), (hx, hy), 2)  # heading line
 
+def draw_pibot_camera(pibot_frame, screen, pos):
+    # --- Draw PiBot camera POV ---
+    if pibot_frame is not None:
+        cam_view = cv2.resize(pibot_frame, (CAM_W, CAM_H))
+        cam_view = cv2.cvtColor(cam_view, cv2.COLOR_BGR2RGB)
+        cam_surf = pygame.surfarray.make_surface(cam_view.swapaxes(0,1))
+        screen.blit(cam_surf, pos)
+
+        # Caption
+        font = pygame.font.SysFont("consolas", 16)
+        label = font.render("PiBot Cam", True, (220, 220, 220))
+        screen.blit(label, (pos[0], pos[1]-20))
 
 def wrap_to_pi(a: float) -> float:
     while a > math.pi:
@@ -313,13 +362,13 @@ if __name__ == "__main__":
 
     # --- Create Robot instance with all required arguments ---
     robot = Robot(ppi, wheels_scale, camera_matrix, camera_dist)
-    fruits_list, fruits_true_pos, aruco_true_pos = read_true_map("image_to_map_generator/generated_ground_truth_maps/final_output.txt")
+    fruits_list, fruits_true_pos, aruco_true_pos = read_true_map(args.map)
     search_list = read_search_list()
     print_target_fruits_pos(search_list, fruits_list, fruits_true_pos)
 
     pygame.init()
     screen = pygame.display.set_mode((WIN_W, WIN_H))
-    pygame.display.set_caption("M4 Fruit Search - Click-to-Go")
+    pygame.display.set_caption("M3 Fruit Search - Click-to-Go")
     clock = pygame.time.Clock()
     robot_pose = Pose(0.0, 0.0, 0.0) 
 
@@ -358,12 +407,26 @@ if __name__ == "__main__":
             goal_xy = None
 
         # Draw map + robot
+    
         screen.fill((10,10,12))
-        draw_map(screen)  # grid + background
-        draw_groundtruth_map(screen, fruits_true_pos, fruits_list, aruco_true_pos)
+        map_pos = (PAD, PAD)
+        draw_map(screen, origin_px = map_pos)  # grid + background
+        
+        draw_groundtruth_map(screen, fruits_true_pos, fruits_list, aruco_true_pos, origin_px = map_pos)
         if goal_xy:
-            draw_goal(screen, goal_xy)
-        draw_robot(screen, robot_pose)
+            draw_goal(screen, goal_xy, origin_px = map_pos)
+        camera_pos = (MAP_W + 2*PAD, PAD)
+        
+        pibot_frame = ppi.get_camera_frame()  # refresh pibot camera
+        draw_pibot_camera(pibot_frame, screen, pos = camera_pos)
+
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
+
+
+    # TODOS
+    ## change parser arg -- DONE
+    ## change GUI so starts with robot front facing north --> DONE
+    ## change waypoints so it is meters based not pixel based
+    # add piBot camera to GUI
